b6866b148e6af9714732404c41e0b9807a260ce0
Sicofonia <sicofonia@hotmail.com>
2020-10-05 11:51:45 +0100

    Console app refactored

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter quantity");
+            var text = Console.ReadLine();
+            var quantity = int.Parse(text);
+            Console.WriteLine("Please enter price");
+            text = Console.ReadLine();
+            var price = decimal.Parse(text);
+            var result = RetailCalculator.TotalAmount(quantity, price);
+            Console.WriteLine($"Order Total = {result}");
         }
     }
 }
